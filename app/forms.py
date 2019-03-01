e2825d8ad531ca4e9b0033c347e19a490ce9afb7
marquin ubuntu virtualbox <marquin@panalysis.com>
2019-03-01 17:21:57 +1100

    added form and database

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, ValidationError
 
 
 class ProjectForm(FlaskForm):
     name = StringField("Project Name", validators=[DataRequired()])
     client = StringField("Client Name", validators=[DataRequired()])
+    submit = SubmitField("Submit New Project")
+
+
+class DetailForm(FlaskForm):
+    stage = StringField("Stage Number", validators=[DataRequired()])
+    description = StringField("Description", validators=[DataRequired()])
+    reason = StringField("Reason")
+    estimate = DecimalField("Estimated Hours", validators=[DataRequired()])
+    comments = StringField("Additional Comments")
+    rate = DecimalField("Hourly Rate ($)", validators=[DataRequired()])
+    submit = SubmitField("Add task to project")
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from app.forms import ProjectForm
+from app.forms import ProjectForm, DetailForm
 from app.models import Project
 
 Bootstrap(app)
@@ -16,5 +16,19 @@ def new_project():
         project = Project(name=form.name.data, client=form.client.data)
         db.session.add(project)
         db.session.commit()
+        return redirect(url_for('create_scopes'))
+    else:
+        print("Form submission failed")
+        flash("Failed to submit data")
 
     return render_template("newproject.html", form=form)
+
+
+@app.route('/createscopes/', methods=['GET', 'POST'])
+def create_scopes():
+
+    form = DetailForm()
+    if form.validate_on_submit():
+        print("success")
+
+    return render_template("createscope.html", form=form)
