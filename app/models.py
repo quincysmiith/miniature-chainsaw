d2d7d4734be2b9b3b2915b415653f9a7fa8791b2
marquin ubuntu virtualbox <marquin@panalysis.com>
2019-03-11 17:17:55 +1100

    adding line model and project id page - incomplete

--- app/models.py
+++ app/models.py
@@ -5,3 +5,17 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
     client = db.Column(db.String(120))
+
+
+class LineItem(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    stage = db.Column(db.String(20))
+    description = db.Column(db.String(500))
+    reason = db.Column(db.String(120))
+    estimate = db.Column(db.Integer)
+    comments = db.Column(db.String(120))
+    rate = db.Column(db.Integer)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+
+    def line_total(self):
+        return self.estimate * self.rate
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask import render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from app.forms import ProjectForm, DetailForm
-from app.models import Project
+from app.models import Project, LineItem
 
 Bootstrap(app)
 
@@ -24,11 +24,24 @@ def new_project():
     return render_template("newproject.html", form=form)
 
 
-@app.route('/createscopes/', methods=['GET', 'POST'])
+@app.route('/project/<project_num>', methods=['GET', 'POST'])
 def create_scopes():
+
+    project = Project.query.filtery_by(id=project_num).first_or_404()
+
+    # need to get all lines with project_id == project_num
 
     form = DetailForm()
     if form.validate_on_submit():
+        line = LineItem(stage=form.stage.data,
+                        description=form.description.data,
+                        reason=form.reason.data,
+                        estimate=form.estimate.data,
+                        comments=form.comments.data,
+                        rate=form.rate.data,
+                        project_id= project_num)
+        db.session.add(line)
+        db.session.commit()
         print("success")
 
     return render_template("createscope.html", form=form)
